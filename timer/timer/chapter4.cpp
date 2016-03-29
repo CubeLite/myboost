870c6dea27d07f00522febde8745b046fcf85d55
fabsnail <fabsnail@163.com>
2016-03-29 14:58:38 +0800

    nocopyable

---
+++ timer/timer/chapter4.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "chapter4.h"
+#include <iostream>
+#include <ostream>
+#include<fstream>
+#include <vector>
+
+#include <boost/typeof/typeof.hpp>
+using namespace boost;
+using namespace std;
+using namespace chapter4;
+
---
+++ timer/timer/chapter4.h
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <boost/noncopyable.hpp>
+#include <boost/utility.hpp>
+
+namespace chapter4
+{
+	class Nocopy : boost::noncopyable
+	{
+
+	};
+}
